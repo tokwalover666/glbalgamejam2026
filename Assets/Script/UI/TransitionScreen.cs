59898f4f54953db6b2020ad8911dbc2f0b1491cf
maia <91291v@gmail.com>
2026-01-31 12:19:27 +0800

    Squashed commit of the following:
    commit f8742d2666f90ce50786bec4fa58d79abcbbf837
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 12:15:55 2026 +0800
    
        Squashed commit of the following:
    
        commit c6613f8f4c47b8e0d90ef19d97f7b6a1f9eaa8fa
        Author: MrBearA <110599403+MrBearA@users.noreply.github.com>
        Date:   Sat Jan 31 10:07:39 2026 +0800
    
            done with package
    
        commit b790ba3de97b50ce78586000813348233b643a12
        Author: MrBearA <110599403+MrBearA@users.noreply.github.com>
        Date:   Sat Jan 31 03:10:31 2026 +0800
    
            updated demo
    
        commit da6d06e6576789c344eab7864d0369e61bc37eb7
        Author: Seju-0 <134390769+Seju-0@users.noreply.github.com>
        Date:   Sat Jan 31 01:11:10 2026 +0800
    
            Larry FBX
    
    commit cb55824eae18a8fe8a9caad71f3154d0a5f1c192
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 12:07:28 2026 +0800
    
        cursor assets
    
    commit 291f1c4da35e379c8b74344d4bdda2be0f9e44dc
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 10:06:39 2026 +0800
    
        long press score and win condition
    
    commit 489d581196a0a26e0c44f6029c50817466ec3b2c
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 07:45:59 2026 +0800
    
        kiss zone gameplay scene
    
    commit f07618fa1856a7bb49e2923427d1ccd566c7083d
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 07:32:51 2026 +0800
    
        long press mechanic
    
    commit 9a9778d28a394bf20d040d35a8fb233967fd9e12
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 04:46:20 2026 +0800
    
        startscreen
    
    commit 11e71125d24edc333b22cf39df010403f150764a
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 04:20:23 2026 +0800
    
        gameplay scene shader
    
    commit a3be71d55ac948b125a0de51c22dc3780045bd19
    Author: maia <91291v@gmail.com>
    Date:   Sat Jan 31 03:12:28 2026 +0800
    
        ui test

---
+++ Assets/Script/UI/TransitionScreen.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using VFX;
+
+public class TransitionScreen : MonoBehaviour
+{
+    [SerializeField] GameObject startScreen;
+    [SerializeField] GameObject zone;
+    [SerializeField] Animator animator;
+
+    private bool isTransitioning = false;
+
+    private void Start()
+    {
+        zone.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0)  && !isTransitioning)
+        {
+            //CameraShake.Shake(0.5f, 1f);
+            Debug.Log("Pressed left click.");
+            StartCoroutine(ClickStart());
+        }
+    }
+
+    public IEnumerator ClickStart()
+    {
+        isTransitioning = true;
+
+        if (startScreen.activeSelf)
+        {
+            animator.SetTrigger("Transition");
+
+            yield return new WaitForSeconds(2.5f);
+
+            zone.SetActive(true);
+            startScreen.SetActive(false);
+        }
+
+        isTransitioning = false;
+    }
+}
